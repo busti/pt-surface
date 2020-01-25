c0a21160055226bcc05ff1c798f1e9be3a351476
busti <mbust@live.de>
2020-01-25 19:21:05 +0100

    initial commit

---
+++ entry.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+linux_version_major = "4.19"
+linux_version_minor = "98"
+linux_version = "v${linux_version_major}${linux_version_minor}"
+
+function second_stage() {
+	mkdir /root/bootstrap
+	( cd /root/bootstrap;
+		sudo apt install build-essential binutils-dev libncurses5-dev libssl-dev ccache bison flex libelf-dev
+		git clone --depth 1 https://github.com/linux-surface/linux-surface linux-surface/
+		git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git linux-kernel/
+		git clone https://github.com/linux-surface/linux-surface-kernel-configs kernel-configs/
+	
+		( cd linux-kernel/;
+			git checkout ${linux_version}
+			git switch -c ${linux_version}-surface
+			for i in /root/bootstrap/linux-surface/patches/${linux_version_major}/*.patch; do patch -p1 < $i; done
+			cp kernel-configs/debian-${linux_version_major}-x86_64.config/ .config
+			make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-linux-surface
+		)
+	
+		dpkg -i linux*.deb
+	)
+}
