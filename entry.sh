6e9873cea218c1f27ed1e8910f8719b921ebe93b
Moritz Bust <mbust@live.de>
2020-01-29 23:33:52 +0100

    setup basic install

--- entry.sh
+++ entry.sh
@@ -1,32 +1,101 @@
 #!/bin/bash
 
-linux_version_major="4.19"
-linux_version_minor="98"
-linux_version="v${linux_version_major}${linux_version_minor}"
+DEVICE="/dev/nvme0n1"
+PART_BOOT="5"
+PART_ROOT="6"
+
+LINUX_VERSION_MAJOR="4.19"
+LINUX_VERSION_MINOR="98"
+LINUX_VERSION="v${LINUX_VERSION_MAJOR}${LINUX_VERSION_MINOR}"
+
+echo -n LUKS_PASSWORD:
+read -s LUKS_PASSWORD
 
 function first_stage() {
-	echo "Hello, World!" #Its tradition
+  set -e
+
+  apt install -y cryptsetup btrfs-progs lvm2
+
+  if [ -d /dev/vg0 ]; then
+    swapoff /dev/vg0/swap || true
+    vgchange -an /dev/vg0
+  fi
+
+  if [ -b /dev/mapper/cryptlvm ]; then
+    cryptsetup luksClose cryptlvm
+  fi
+
+  (
+    echo n   # add a new partition         :  boot
+    echo     # partition number            => count + 1
+    echo     # first sector                => after last
+    echo +2G # last sector                 => 2GB size
+    echo Y   # delete filesystem signature => [Y]es
+    echo n   # add a new partition         :  root
+    echo     # partition number            => count + 1
+    echo     # first sector                => after last
+    echo     # last sector                 => fill remaining space
+    echo Y   # delete filesystem signature => [Y]es
+    echo w   # write changes to disk
+  ) | fdisk ${DEVICE}
+
+  lsblk
+
+  echo mkfs.ext4 ${DEVICE}${PART_BOOT}
+  yes | mkfs.ext4 ${DEVICE}${PART_BOOT}
+
+  (
+    echo $LUKS_PASSWORD
+    echo $LUKS_PASSWORD
+  ) | cryptsetup luksFormat ${DEVICE}${PART_ROOT}
+  echo $LUKS_PASSWORD | cryptsetup luksOpen ${DEVICE}${PART_ROOT} cryptlvm
+  pvcreate /dev/mapper/cryptlvm
+  vgcreate vg0 /dev/mapper/cryptlvm
+
+  echo lvcreate /dev/vg0 --name=root --size=100G
+  lvcreate /dev/vg0 --name=root --size=100G
+  yes | mkfs.btrfs /dev/vg0/root
+
+  echo lvcreate /dev/vg0 --name=swap --size=8G
+  lvcreate /dev/vg0 --name=swap --size=8G
+  mkswap /dev/vg0/swap
+  swapon /dev/vg0/swap
+
+  echo mount /dev/vg0/root $TARGET
+  mount /dev/vg0/root $TARGET
+
+  echo debootstrap sid $TARGET
+  debootstrap sid $TARGET
+
+  mount ${DEVICE}${PART_BOOT} $TARGET/boot
+  echo mount ${DEVICE}${PART_BOOT} $TARGET/boot
+
+  genfstab -U $TARGET | tee $TARGET/etc/fstab
+  UUID=$(lsblk -lpo NAME,UUID | grep ${DEVICE}${PART_ROOT} | awk '{print $2}')
+  echo cryptlvm UUID=${UUID} none luks >$TARGET/etc/crypttab
 }
 
 function second_stage() {
-	set -e
-	apt install -y git
-	
-	mkdir -p /root/bootstrap
-	( cd /root/bootstrap;
-		apt install -y build-essential binutils-dev libncurses5-dev libssl-dev ccache bison flex libelf-dev
-		git clone --depth 1 https://github.com/linux-surface/linux-surface linux-surface/
-		git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git linux-kernel/
-		git clone https://github.com/linux-surface/linux-surface-kernel-configs kernel-configs/
-	
-		( cd linux-kernel/;
-			git checkout ${linux_version}
-			git switch -c ${linux_version}-surface
-			for i in /root/bootstrap/linux-surface/patches/${linux_version_major}/*.patch; do patch -p1 < $i; done
-			cp kernel-configs/debian-${linux_version_major}-x86_64.config/ .config
-			make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-linux-surface
-		)
-	
-		dpkg -i linux*.deb
-	)
+  set -e
+  apt install -y git
+
+  mkdir -p /root/bootstrap
+  (
+    cd /root/bootstrap
+    apt install -y build-essential binutils-dev libncurses5-dev libssl-dev ccache bison flex libelf-dev
+    git clone --depth 1 https://github.com/linux-surface/linux-surface linux-surface/
+    git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git linux-kernel/
+    git clone https://github.com/linux-surface/linux-surface-kernel-configs kernel-configs/
+
+    (
+      cd linux-kernel/
+      git checkout ${LINUX_VERSION}
+      git switch -c ${LINUX_VERSION}-surface
+      for i in /root/bootstrap/linux-surface/patches/${LINUX_VERSION_MAJOR}/*.patch; do patch -p1 <$i; done
+      cp kernel-configs/debian-${LINUX_VERSION_MAJOR}-x86_64.config/ .config
+      make -j $(getconf _NPROCESSORS_ONLN) deb-pkg LOCALVERSION=-linux-surface
+    )
+
+    dpkg -i linux*.deb
+  )
 }
